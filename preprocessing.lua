3ee1263bac2889c3b4543dbf6fa7bcfabbbf027c
Mark Liu <ml4133@compute-12-5.local>
2015-03-07 14:06:15 -0500

    constructing whitening matrix

--- preprocessing.lua
+++ preprocessing.lua
@@ -20,10 +20,39 @@ tensor:float()
 -- Random Patch Selection 
 
 random_patches = torch.FloatTensor(random_sample_count, image_channels, receptive_field_size, receptive_field_size)
-for i = 1, random_sample_count do
-    image = tensor[math.random(1, image_count)]
+patch_count = 0
+while patch_count < random_sample_count do
+    image_index = math.random(1, image_count)
+    image = tensor[image_index]
     patch_x = math.random(1, image_width - receptive_field_size + 1)
     patch_y = math.random(1, image_height - receptive_field_size + 1)
     patch = image[{{}, {patch_x, patch_x + receptive_field_size - 1}, {patch_y, patch_y + receptive_field_size - 1}}]
-    random_patches[i] = patch
+    patch_count = patch_count + 1
+    random_patches[patch_count] = patch
+    mean = random_patches[{patch_count, {}, {}, {}}]:mean()
+    std = random_patches[{patch_count, {}, {}, {}}]:std()
+    if std == 0 then
+       print(string.format('Discarding patch from img %d, x:%d, y:%d for 0 std', image_index, patch_x, patch_y))
+       goto continue
+    end
+    random_patches[{patch_count, {}, {}, {}}]:add(-mean)
+    random_patches[{patch_count, {}, {}, {}}]:div(std)
+    ::continue::
 end
+
+-- Whitening
+
+patch_size = image_channels*receptive_field_size*receptive_field_size
+random_patches_flattened = torch.reshape(random_patches, random_sample_count, patch_size)
+means = torch.mean(random_patches_flattened, 1)
+random_patches_flattened:add(means:expand(random_sample_count, patch_size))
+covariances = torch.mm(random_patches_flattened:transpose(1,2), random_patches_flattened):div(random_sample_count)
+Q, D, Q_T = torch.svd(covariances)
+D_inv_sqrt = torch.pow(D, -1):sqrt():resize(patch_size, 1)
+D_isQ_t = Q_T:cmul(D_inv_sqrt:expand(patch_size, patch_size))
+W_zca = torch.mm(Q, D_isQ_t)
+
+whitened = torch.mm(W_zca, random_patches_flattened)
+
+-- k-means
+
