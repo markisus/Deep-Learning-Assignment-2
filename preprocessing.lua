f9590a065abde4347f129402b6fd03060b8b8693
Mark Liu <ml4133@login-0-1.local>
2015-03-06 23:51:17 -0500

    loading binary data, converting to tensor, picking a random patch

---
+++ preprocessing.lua
@@ -0,0 +1,25 @@
+require 'nn'
+require 'torch'
+require 'math'
+
+data_path = '/scratch/courses/DSGA1008/A2/binary/unlabeled_X.bin'
+image_count = 100000
+image_width = 96
+image_height = 96
+image_channels = 3
+
+random_sample_count = 1000
+receptive_field_size = 10
+
+data = torch.DiskFile(data_path, 'r', true)
+data:binary():littleEndianEncoding()
+tensor = torch.ByteTensor(image_count, image_channels, image_width, image_height)
+data:readByte(tensor:storage())
+tensor:float()
+
+-- Random Patch Selection 
+
+image = tensor[math.random(1, image_count)]
+patch_x = math.random(1, image_width - receptive_field_size + 1)
+patch_y = math.random(1, image_height - receptive_field_size + 1)
+patch = image[{{}, {patch_x, patch_x + receptive_field_size - 1}, {patch_y, patch_y + receptive_field_size - 1}}]
